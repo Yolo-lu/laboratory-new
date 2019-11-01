d0451933851f7679867d454e10bedfc6a8ca3fcf
Yolo-Lu <842105661@qq.com>
2019-11-01 16:26:48 +0800

    SHIYAN

---
+++ one/VUE.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    VUE
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    name: "VUE",
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+    methods: {},
+
+    mounted() {
+
+    },
+    created() {
+
+    },
+    filters: {},
+    computed: {},
+    watch: {},
+    directives: {}
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
